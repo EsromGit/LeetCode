4e863f7073f5e5a872ea9d70caccd28be52864a2
Esrom Tesfay <154343032+EsromGit@users.noreply.github.com>
2024-10-03 13:42:46 -0400

    Time: 217 ms (5.12%), Space: 16.6 MB (63.97%) - LeetHub

--- 0397-integer-replacement/0397-integer-replacement.py
+++ 0397-integer-replacement/0397-integer-replacement.py
@@ -1,9 +1,16 @@
 class Solution:
     def integerReplacement(self, n: int) -> int:
+        cache = {}
+        if n in cache:
+            return cache[n]
+    
         if n == 1:
             return 0
         
-        if n%2 == 0:
-            return 1 + self.integerReplacement(n//2)
+        if n % 2 == 0:
+            value = 1+ self.integerReplacement(n //2)
         else:
-            return 1 + min(self.integerReplacement(n + 1), self.integerReplacement(n - 1))
+            value = 1 + min(self.integerReplacement(n + 1), self.integerReplacement(n - 1))
+
+        cache[n] = value
+        return value

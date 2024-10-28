325286ebaabe9877db1d377b0a16c1331880f5a3
Esrom Tesfay <154343032+EsromGit@users.noreply.github.com>
2024-10-27 20:13:35 -0400

    Time: 3 ms (94.77%), Space: 17 MB (39.25%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -0,0 +1,14 @@
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        counter = 0
+        pointer = 0
+        while pointer < len(flowerbed):
+            if (flowerbed[pointer] == 0 and 
+            (pointer == 0 or flowerbed[pointer - 1] == 0) and 
+            (pointer == len(flowerbed)-1 or flowerbed[pointer + 1] == 0)):
+                flowerbed[pointer] = 1
+                counter += 1
+                pointer += 2
+            else:
+                pointer += 1
+        return counter >= n      

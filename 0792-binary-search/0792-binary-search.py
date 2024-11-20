dc2d481e1e4da7402a5396be1926f7319664ebce
Esrom Tesfay <154343032+EsromGit@users.noreply.github.com>
2024-11-20 11:28:44 -0500

    Time: 1 ms (9.95%), Space: 17.6 MB (96.68%) - LeetHub

--- 0792-binary-search/0792-binary-search.py
+++ 0792-binary-search/0792-binary-search.py
@@ -1,19 +1,8 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        #Sorted Array
-        #Find target in Array
-
-        #Want to calulate the mid point, get the index if midpoint is equal to target
-
-        left = 0
-        right = len(nums) - 1
-
-        while left <= right:
-            m = (left+right) // 2
-            if nums[m] < target:
-                left = m + 1
-            elif nums[m] > target:
-                right = m - 1
-            else:
-                return m 
+        for i in range(len(nums)):
+            if nums[i] == target:
+                return i
         return -1
+
+        
